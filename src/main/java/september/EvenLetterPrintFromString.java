c8ca02ff143424585e1abf489a7136b35aa7bff6
testingnewprofile10 <iaqudas10@gmail.com>
2025-09-21 21:34:32 +0530

    added one new program

---
+++ src/main/java/september/EvenLetterPrintFromString.java
@@ -0,0 +1,25 @@
+package september;
+
+import java.util.Scanner;
+
+public class EvenLetterPrintFromString {
+	public static void main(String[] args) {
+
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter the Words: ");
+
+		String input = sc.nextLine();
+
+		for (int i = 1; i <= input.length() - 1; i += 2) {
+
+		System.out.println(input.charAt(i));
+
+		}
+
+		}
+
+		} 
+
